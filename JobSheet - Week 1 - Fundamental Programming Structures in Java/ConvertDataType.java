3f6bcbb7ca79e57beed3d33b9604f3acdcdecabe
yayyzea <azkiyaz65@gmail.com>
2026-02-12 23:31:33 +0700

    Add files via upload

---
+++ JobSheet - Week 1 - Fundamental Programming Structures in Java/ConvertDataType.java
@@ -0,0 +1,15 @@
+package w1;
+
+/**
+ *
+ * @author ZAHRA
+ */
+class ConvertDataType {
+static short methodOne(long l) {
+int i = (int) l; return (short)i;
+}
+public static void main(String[] args) {
+double d = 10.25; float f = (float) d;
+byte b = (byte) methodOne((long) f); System.out.println(b);
+}
+}
---
+++ JobSheet - Week 1 - Fundamental Programming Structures in Java/FloatingPoint.java
@@ -0,0 +1,12 @@
+package w1;
+
+/**
+ *
+ * @author ZAHRA
+ */
+class FloatingPoint {
+public static void main(String[] args) {
+double x = 92.98;
+int nx = (int) Math.round(x);
+}
+}
